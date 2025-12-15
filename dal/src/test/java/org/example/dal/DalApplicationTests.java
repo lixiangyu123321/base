d15ee8d537088d047cc7fa2a2e7f906eeed21c31
11188813 <2809892243@qq.com>
2025-12-15 14:53:21 +0800

    init

---
+++ dal/src/test/java/org/example/dal/DalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ facade/src/test/java/org/example/facade/FacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.facade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
